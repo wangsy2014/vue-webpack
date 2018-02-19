abb2885010fdb6511a899fb42a5977c69c41dada
123 <461181768@qq.com>
2018-02-19 16:42:05 +0800

    上传照片 前端页面展示

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,77 @@
 <template>
   <div>
     {{kk}}
+
+    <el-form :model="formValue">
+          <el-input v-model="msg" style="width:400px" placeholder="123"></el-input>
+      <el-form-item label="">
+        <el-upload class="upload-demo" action="/api"
+        :on-success ="handleUpload"
+        :on-error ="errorUpload"
+        :on-preview="handlePreview" :on-remove="handleRemove" :before-remove="beforeRemove" multiple :limit="3" :on-exceed="handleExceed" :file-list="fileList">
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+
+    <el-button type="" @click="testUrl">这个是测试</el-button>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      kk: '123'
+      kk: '123',
+      formValue: {
+        a: 1,
+        b: 2
+      },
+      fileList: []
+    }
+  },
+  methods: {
+    handleRemove (file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePreview (file) {
+      console.log(file)
+    },
+    handleExceed (files, fileList) {
+      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
+    },
+    beforeRemove (file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`)
+    },
+    handleUpload (res, file, fileList) {
+      if (res.result === true) {
+        // 成功之后弹框，上传成功
+        this.$message({
+          message: '恭喜你，上传成功',
+          type: 'success'
+        })
+      }
+      console.error()
+      console.error(file)
+      console.error(fileList)
+    },
+    errorUpload (err, file, fileList) {
+      console.error(err)
+      console.error(file)
+      console.error(fileList)
+    },
+    testUrl () {
+      axios.get('/api').then((req, res) => {
+        console.error(req)
+        console.error(res)
+      }).catch(err => {
+        console.error(err)
+      })
     }
   }
 }
